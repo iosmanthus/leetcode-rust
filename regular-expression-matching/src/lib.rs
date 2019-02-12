60f68a607a3432761ff18eb30ebd8a788b37b610
iosmanthus <myosmanthustree@gmail.com>
2019-02-12 15:38:06 +0800

    apply dynamic programming

--- regular-expression-matching/src/lib.rs
+++ regular-expression-matching/src/lib.rs
@@ -1,25 +1,27 @@
 pub struct Solution {}
 impl Solution {
     pub fn is_match(text: String, pattern: String) -> bool {
-        let mut matched = vec![vec![false; pattern.len() + 1]; text.len() + 1];
+        let mut matched = vec![vec![false; pattern.len() + 1]; 2];
+        let (mut now, mut past) = (0, 1);
         for i in (0..text.len() + 1).rev() {
             for j in (0..pattern.len() + 1).rev() {
                 let (text, pattern) = (&text[i..], &pattern[j..]);
-                matched[i][j] = if pattern.is_empty() {
+                matched[now][j] = if pattern.is_empty() {
                     text.is_empty()
                 } else {
                     let first_match = !text.is_empty()
                         && (text.as_bytes()[0] == pattern.as_bytes()[0]
                             || pattern.as_bytes()[0] == b'.');
                     if pattern.len() >= 2 && pattern.as_bytes()[1] == b'*' {
-                        (first_match && matched[i + 1][j]) || matched[i][j + 2]
+                        (first_match && matched[past][j]) || matched[now][j + 2]
                     } else {
-                        first_match && matched[i + 1][j + 1]
+                        first_match && matched[past][j + 1]
                     }
-                }
+                };
             }
+            std::mem::swap(&mut now, &mut past);
         }
-        matched[0][0]
+        matched[past][0]
     }
 }
 #[cfg(test)]
@@ -37,6 +39,11 @@ mod tests {
         assert_eq!(
             true,
             Solution::is_match("asdfjkl;".to_string(), ".*".to_string())
+        );
+        assert_eq!(false, Solution::is_match("aa".to_string(), "a".to_string()));
+        assert_eq!(
+            true,
+            Solution::is_match("aab".to_string(), "c*a*b".to_string())
         );
     }
 }
