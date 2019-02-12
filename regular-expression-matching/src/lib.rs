bb7675cff9391f26d6ba9be04b96388e14de30c7
iosmanthus <myosmanthustree@gmail.com>
2019-02-12 12:30:20 +0800

    apply dynamic programming

--- regular-expression-matching/src/lib.rs
+++ regular-expression-matching/src/lib.rs
@@ -1,21 +1,25 @@
 pub struct Solution {}
 impl Solution {
-    pub fn is_match(string: String, pattern: String) -> bool {
-        let sbytes = string.as_bytes();
-        let pbytes = pattern.as_bytes();
-
-        if pattern.is_empty() {
-            string.is_empty()
-        } else {
-            let first_match = !string.is_empty() && (sbytes[0] == pbytes[0] || pbytes[0] == b'.');
-            if pattern.len() >= 2 && pbytes[1] == b'*' {
-                (first_match
-                    && Solution::is_match(string[1..].to_string(), pattern[..].to_string()))
-                    || Solution::is_match(string[..].to_string(), pattern[2..].to_string())
-            } else {
-                first_match && Solution::is_match(string[1..].to_string(), pattern[1..].to_string())
+    pub fn is_match(text: String, pattern: String) -> bool {
+        let mut matched = vec![vec![false; pattern.len() + 1]; text.len() + 1];
+        for i in (0..text.len() + 1).rev() {
+            for j in (0..pattern.len() + 1).rev() {
+                let (text, pattern) = (&text[i..], &pattern[j..]);
+                if pattern.is_empty() {
+                    matched[i][j] = text.is_empty();
+                } else {
+                    let first_match = !text.is_empty()
+                        && (text.as_bytes()[0] == pattern.as_bytes()[0]
+                            || pattern.as_bytes()[0] == b'.');
+                    if pattern.len() >= 2 && pattern.as_bytes()[1] == b'*' {
+                        matched[i][j] = (first_match && matched[i + 1][j]) || matched[i][j + 2]
+                    } else {
+                        matched[i][j] = first_match && matched[i + 1][j + 1]
+                    }
+                }
             }
         }
+        matched[0][0]
     }
 }
 #[cfg(test)]
@@ -27,6 +31,7 @@ mod tests {
             false,
             Solution::is_match("ab".to_string(), ".*c".to_string())
         );
+
         assert_eq!(true, Solution::is_match("aa".to_string(), "a*".to_string()));
 
         assert_eq!(
