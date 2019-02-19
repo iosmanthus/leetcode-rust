976b053d41f5e795ffe6c51b6123992613cbfc41
iosmanthus <myosmanthustree@gmail.com>
2019-02-19 22:18:53 +0800

    add a safe implementation

--- remove-nth-node-from-end-of-list/src/lib.rs
+++ remove-nth-node-from-end-of-list/src/lib.rs
@@ -14,6 +14,32 @@ impl ListNode {
 
 pub struct Solution {}
 impl Solution {
+    #[cfg(feature = "safe")]
+    pub fn remove_nth_from_end(list: Option<Box<ListNode>>, n: i32) -> Option<Box<ListNode>> {
+        pub fn remove(list: Option<Box<ListNode>>, n: i32) -> (Option<Box<ListNode>>, i32) {
+            match list {
+                Some(node) => {
+                    let (rest, tail_index) = remove(node.next, n);
+                    let tail_index = tail_index + 1;
+                    (
+                        if tail_index == n {
+                            rest
+                        } else {
+                            Some(Box::new(ListNode {
+                                val: node.val,
+                                next: rest,
+                            }))
+                        },
+                        tail_index,
+                    )
+                }
+                None => (None, 0),
+            }
+        }
+        remove(list, n).0
+    }
+
+    #[cfg(feature = "unsafe")]
     pub fn remove_nth_from_end(list: Option<Box<ListNode>>, n: i32) -> Option<Box<ListNode>> {
         let mut list = Some(Box::new(ListNode { val: 0, next: list }));
         unsafe {
