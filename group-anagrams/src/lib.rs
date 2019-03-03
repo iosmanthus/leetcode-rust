3b9175ad8eeb46e456771701bea2be25bf50d8bf
iosmanthus <myosmanthustree@gmail.com>
2019-03-03 10:48:44 +0800

    easy

---
+++ group-anagrams/src/lib.rs
@@ -0,0 +1,34 @@
+pub struct Solution {}
+impl Solution {
+    fn hash(string: &str) -> [usize; 26] {
+        let mut alphabet = [0; 26];
+        string.chars().for_each(|ch| {
+            alphabet[ch as usize - 'a' as usize] += 1;
+        });
+        alphabet
+    }
+    pub fn group_anagrams(strs: Vec<String>) -> Vec<Vec<String>> {
+        use std::collections::HashMap;
+        let mut mapping = HashMap::new();
+        strs.into_iter().for_each(|string| {
+            mapping
+                .entry(Self::hash(&string))
+                .or_insert_with(|| vec![])
+                .push(string);
+        });
+        mapping.values().cloned().collect()
+    }
+}
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn it_works() {
+        dbg!(Solution::group_anagrams(
+            ["eat", "tea", "tan", "ate", "nat", "bat"]
+                .into_iter()
+                .map(|slice| slice.to_string())
+                .collect()
+        ));
+    }
+}
